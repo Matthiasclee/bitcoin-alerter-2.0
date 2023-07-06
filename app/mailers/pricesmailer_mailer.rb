192f834129928f0817e0725157d3ab5e6d875fab
Matthias Lee <matthias@matthiasclee.com>
2023-07-06 08:17:13 -0700

    Add mailer

---
+++ app/mailers/pricesmailer_mailer.rb
@@ -0,0 +1,2 @@
+class PricesmailerMailer < ApplicationMailer
+end
---
+++ test/mailers/pricesmailer_mailer_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PricesmailerMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
