98ac86c714522e59264d1bbdad666ca98819aee5
Matthias Lee <matthias@matthiasclee.com>
2023-07-06 08:23:57 -0700

    Remove messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_03_130127) do
+ActiveRecord::Schema.define(version: 2023_07_06_152335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "messages", force: :cascade do |t|
-    t.string "content"
-    t.string "to"
-    t.string "from"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "phone", null: false
