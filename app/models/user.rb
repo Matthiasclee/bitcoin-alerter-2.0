8bf616714cad9b16bd601dfa32eae5fc1547e0bb
Matthias Lee <matthias@matthiasclee.com>
2023-07-06 08:29:44 -0700

    Message stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,33 +2,22 @@ class User < ApplicationRecord
   require 'net/http'
   require 'active_support/core_ext/numeric/conversions'
 
-  def prices
-    prices = {}
-
+  def message_data
     resp = JSON.parse(Net::HTTP.get(URI("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD")))
 
     if resp["Response"] != "Error"
       price = resp["USD"].to_f.to_i
-      prices[c.to_sym] = price
     else
-      prices[c.to_sym] = -1
+      price = -1
     end
 
-    return prices
-  end
-
-  def message_data
-    dat = ""
-
-    prices.each do |p|
-      if p[1]>-1
-        dat << "#{p[0]}: $#{p[1].to_s(:delimited)}\n"
-      else
-        dat << "#{p[0]}: Sorry, something went wrong fetching this price."
-      end
+    if p[1]>-1
+      message_data = "$#{p[1].to_s(:delimited)}"
+    else
+      message_data = "Sorry, something went wrong fetching this price."
     end
 
-    return dat.chomp
+    return message_data
   end
 
   def send_message
