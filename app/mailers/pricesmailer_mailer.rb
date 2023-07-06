265d10d6a03b80c3b2d58329427a46947432b53b
Matthias Lee <matthias@matthiasclee.com>
2023-07-06 08:22:19 -0700

    Add prices mailer

--- app/mailers/pricesmailer_mailer.rb
+++ app/mailers/pricesmailer_mailer.rb
@@ -1,2 +1,12 @@
 class PricesmailerMailer < ApplicationMailer
+  def generate_message(phone:, carrier:, price:)
+    gateways = ["@vtext.com", "@txt.att.net", "@tmomail.net", "@messaging.sprintpcs.com", "@mymetropcs.com", "@sms.cricketwireless.net", "@msg.fi.google.com"]
+    mail(
+      from: "bitcoinalerter@matthiasclee.com",
+      to: "#{phone}@#{gateways[carrier]}",
+      subject: "BTC",
+      body: price,
+      content_type: "text/plain"
+    )
+  end
 end
